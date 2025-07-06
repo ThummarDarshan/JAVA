db95bee9d2755255b617cae618be8058b25e9744
ThummarDarshan <darshantce.059@gmail.com>
2025-07-06 19:05:57 +0530

    Add files via upload

---
+++ online/patten.java
@@ -0,0 +1,13 @@
+public class patten{
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<5;i++)
+        {
+           for(int k=0;k<=i;k++)
+           {
+                System.out.print(k +1);
+           }
+              System.out.println();  
+        }
+    }
+}
---
+++ online/sum.java
@@ -0,0 +1,48 @@
+/*
+  next take only single word input
+  nextline() take all the input till the end of line
+  nextInt() takes only integer input
+  nextDouble() takes only double input
+  nextBoolean() takes only boolean input
+  nextfloat() takes only float input
+  next() takes only string input
+
+  sout = System.out.println("");  is used to print output
+  sc is used to take input from user
+
+  println() is used to print output in new line
+  print() is used to print output in same line
+  \n is used to print output in new line
+  \t is used to print output in tab space
+ */
+
+
+
+import java.util.*;
+
+
+class add
+{
+     int Sum(int a,int b)
+    {
+        return a+b;
+    }
+}
+
+
+public class sum {
+    public static void main(String[] args) {
+       int a,b,sum;
+
+       Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st Number : ");
+        a = sc.nextInt();
+        System.out.println("Enter 2nd Number : ");
+        b = sc.nextInt();
+
+       add s = new add();
+
+        System.out.println("Sum is: " + s.Sum(a,b));
+    }
+}
