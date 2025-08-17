db314720906dbd2347e316fb78fc74b2164edc32
ThummarDarshan <darshantce.059@gmail.com>
2025-08-17 18:51:26 +0530

    Create array.java

---
+++ online/array.java
@@ -0,0 +1,26 @@
+public class array{
+
+  public static void main(String args[]){
+
+  /*
+    // static array
+    int nums[] = {2,5,7,8};
+    nums[1] = 6;
+
+    System.out.println(nums[1]);*/
+
+    // dynamic array 
+
+    int nums[] = nem int[4];
+    nums[0] = 4;
+    nums[1] = 5;
+    nums[2] = 6;
+    nums[3] = 7;
+
+    for(int i=0 ; i<4 ; i++)
+    {
+      System.out.println(nums[i]);
+    }
+  }
+
+}
