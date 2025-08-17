15dd4aacf81e59596a15518fc13d99cd276ff64f
ThummarDarshan <darshantce.059@gmail.com>
2025-08-17 18:16:05 +0530

    Create methodoverloading.java

---
+++ online/methodoverloading.java
@@ -0,0 +1,29 @@
+classs calculator
+{
+  
+  public int add(int n1, int n2 ,int n3)
+  {
+    return n1+n2+n3;    
+  }
+  
+  public int add(int n1,int n2)
+  {
+    return n1+n2;
+  }
+  
+  public double add(double n1,int n2)
+  {
+    return n1+n2;
+  }
+}
+
+public class methodoverloading {
+
+  public static void main(String arrs[])
+  {
+      calculator obj = new calculator();
+      int r1 = obj . add(2,4);
+      system.out.println(s1);
+  }
+
+}
