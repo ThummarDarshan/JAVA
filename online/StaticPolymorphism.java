85c681fdcc8dc900812f516e821748551e25326e
ThummarDarshan <darshantce.059@gmail.com>
2025-08-18 02:00:33 +0530

    Create StaticPolymorphism.java

---
+++ online/StaticPolymorphism.java
@@ -0,0 +1,17 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(2, 3));       // calls int version
+        System.out.println(calc.add(2.5, 3.5));  // calls double version
+    }
+}
